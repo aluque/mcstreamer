0489f0dedd0870d0ed14b7db066922527998cec0
Alejandro Luque <aluque@iaa.es>
2022-11-10 16:24:04 +0100

    Improvements to plotting.

--- MCStreamer/src/plot.jl
+++ MCStreamer/src/plot.jl
@@ -2,8 +2,13 @@
   Plotting functions.
 =#
 
-function plot(fields::GridFields; rlim=nothing, zlim=nothing, kw...)
+function plot(fields::GridFields; titleprefix="", rlim=nothing, zlim=nothing,
+              savedir=nothing, kw...)
     plt.matplotlib.pyplot.style.use("granada")
+    if !isnothing(savedir)
+        isdir(savedir) || mkpath(savedir)
+    end
+
     M = fields.grid.M
     N = fields.grid.N
     # qfixed_t = dropdims(sum(@view(fields.qfixed[1:M, 1:N, :]), dims=3), dims=3)
@@ -12,38 +17,78 @@ function plot(fields::GridFields; rlim=nothing, zlim=nothing, kw...)
     # q = qfixed_t .- qpart_t
     @unpack rf, zf, rc, zc = fields.grid
     
+    function xylabel()
+        plt.xlabel("z (mm)")
+        plt.ylabel("r (mm)")
+    end
+    
     (i1, i2) = indexlims(rlim, rc, M)
     (j1, j2) = indexlims(zlim, zc, N)
 
-    plt.figure("Charge density")
+    plt.figure("$titleprefix Charge density")
     q = @view(fields.q[i1:i2, j1:j2])
     qmax, qmin = extrema(q)
     absmax = max(abs(qmax), abs(qmin)) * co.elementary_charge
     
-    plt.pcolormesh(zf[j1:(j2 + 1)], rf[i1:(i2 + 1)], q .* co.elementary_charge;
+    plt.pcolormesh(zf[j1:(j2 + 1)] ./ co.milli,
+                   rf[i1:(i2 + 1)] ./ co.milli,
+                   q .* co.elementary_charge;
                    cmap="seismic", vmin=-absmax, vmax=absmax, kw...)
     cbar = plt.colorbar(label=L"Charge density (C/m$^3$)")
-    setlims(rlim, zlim)
+
+    if rlim != nothing && zlim!= nothing
+        setlims(rlim / co.milli, zlim / co.milli)
+    end
     
-    plt.figure("Electric field")
+    xylabel()
+    
+    if !isnothing(savedir)
+        fname = joinpath(savedir, "charge.png")
+        @info "Saving plot to" fname
+        plt.savefig(fname, dpi=600)
+    end
+    
+    plt.figure("$titleprefix Electric field")
     eabs = @. @views sqrt(0.25 * (fields.er[i1:i2, j1:j2] + fields.er[(i1 + 1):(i2 + 1), j1:j2])^2 +
                           0.25 * (fields.ez[i1:i2, j1:j2] + fields.ez[i1:i2, (j1 + 1):(j2 + 1)])^2)
 
-    plt.pcolormesh(zf[j1:(j2 + 1)], rf[i1:(i2 + 1)], eabs, cmap="gnuplot2"; kw...)
+    plt.pcolormesh(zf[j1:(j2 + 1)] ./ co.milli,
+                   rf[i1:(i2 + 1)] ./ co.milli,
+                   eabs, cmap="gnuplot2"; kw...)
     cbar = plt.colorbar(label="Electric field (V/m)")
-    setlims(rlim, zlim)
+    if rlim != nothing && zlim!= nothing
+        setlims(rlim / co.milli, zlim / co.milli)
+    end
+    xylabel()
 
-    plt.figure("Electron density")
+    if !isnothing(savedir)
+        fname = joinpath(savedir, "efield.png")
+        @info "Saving plot to" fname
+        plt.savefig(fname, dpi=600)
+    end
+
+    plt.figure("$titleprefix Electron density")
     ne = @views -dropdims(sum(fields.qpart[i1:i2, j1:j2, begin:end], dims=3), dims=3)
     lognorm = plt.matplotlib.colors.LogNorm(vmin=1e15, vmax=1e21)
-    plt.pcolormesh(zf[j1:(j2 + 1)], rf[i1:(i2 + 1)], ne, cmap="gnuplot2", norm=lognorm; kw...)
+    plt.pcolormesh(zf[j1:(j2 + 1)] ./ co.milli,
+                   rf[i1:(i2 + 1)] ./ co.milli,
+                   ne, cmap="gnuplot2", norm=lognorm; kw...)
     cbar = plt.colorbar(label="Electron density (m\$^{-3}\$)")
-    setlims(rlim, zlim)
-
-    setlims(rlim, zlim)
-
+    if rlim != nothing && zlim!= nothing
+        setlims(rlim / co.milli, zlim / co.milli)
+    end
+    xylabel()
     
-    plt.show()
+    
+    if !isnothing(savedir)
+        fname = joinpath(savedir, "edensity.png")
+        @info "Saving plot to" fname
+        plt.savefig(fname, dpi=600)
+    end
+    
+    if isnothing(savedir)
+        plt.show()
+    end
 end
 
 function indexlims(lim::AbstractVector, x, n)
@@ -57,9 +102,12 @@ function indexlims(lim::Nothing, x, n)
 end
 
 
-function plot(fname::String; kw...)
+function plot(fname::String; save=false, kw...)
     fields = load(fname, "fields");
-    plot(fields; kw...)
+
+    savedir = save ? splitext(fname)[1] : nothing
+
+    plot(fields; savedir, kw...)
 end
 
 function setlims(rlim, zlim)
